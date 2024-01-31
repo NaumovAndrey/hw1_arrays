9575d7fcc7bc5851077edd792e06f762e500f068
andrey <naumov.a.n@icloud.com>
2024-01-31 22:53:25 +0700

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "iostream"
+#include <conio.h>
+#include <clocale>
+#include "iostream"
+
+using namespace std;
+
+void task1();
+void task2();
+void task3();
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    cout << "Домашняя работа к gпервой лекции. Одномерные массивы. Базовые алгоритмы на массивах" << endl;
+
+
+    while (true)
+    {
+        cout << "1: Многоквартирный дом." << endl;
+        cout << "2: Сортировка." << endl;
+        cout << "3: Использование assert." << endl;
+        
+        int task;
+        cin >> task;
+
+        if (task == 1) {
+            system("cls");
+            cout << "Многоквартирный дом." << endl;
+            task1();
+            _getch();
+            system("cls");
+        }
+
+        else if (task == 2) {
+            system("cls");
+            cout << "Сортировка." << endl;
+            task2();
+            _getch();
+            system("cls");;
+        }
+
+        else if (task == 3) {
+            system("cls");
+            cout << "Использование assert." << endl;
+            task3();
+            _getch();
+            system("cls");
+        }
+
+        else
+        {
+            system("cls");
+            cout << "некорректный ввод. Выход из программы";
+            break;
+        }
+    }
+
+}
---
+++ task3.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <clocale>
+#include "iostream"
+
+using namespace std;
+
+
+/*Создайте в своей программе функцию float travelTime(float distance, float speed). 
+Она будет находить время в пути и при этом использовать assert, чтобы отбросить нулевую и отрицательную скорость. 
+Вызовите эту функцию в main. Убедитесь, что падение происходит за счёт assert. 
+Не забудьте подключить заголовочный файл cassert.*/
+
+void task3() {
+    
+}
