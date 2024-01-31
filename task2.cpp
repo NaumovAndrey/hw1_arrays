415651f78e39580c7b802b4863ddaeb841f98f4f
andrey <naumov.a.n@icloud.com>
2024-02-01 00:22:56 +0700

    update task2

--- task2.cpp
+++ task2.cpp
@@ -10,5 +10,28 @@ using namespace std;
 По возможности используйте минимум дополнительной памяти и проходов по массиву.*/
 
 void task2() {
+    const int size = 15;
+    float numbers[size];
 
+    cout << "Введите 15 дробных чисел: " << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (numbers[j] < numbers[j + 1]) {
+                float temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Дробные числа в порядке убывания: " << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << numbers[i] << "| ";
+    }
+    cout << endl;
 }
